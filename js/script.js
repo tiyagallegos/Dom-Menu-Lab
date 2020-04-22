1b3b32f25e25e2e2db6b944083712e42dc1b49f1
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-04-21 22:19:06 -0600

    -m "tasks 1.1 to 1.2"

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-let mainEl = document.getElementsByTagName('main');
+//Task 1.1
+
+let mainEl = document.querySelector('main');
 mainEl.style.backGroundColor = 'var(--main-bg)';
+
+//Task 1.2
+
+mainEl.document.innerHTML = <h1>SEI Rocks!</h1>
+
+//Task 1.3 
+
