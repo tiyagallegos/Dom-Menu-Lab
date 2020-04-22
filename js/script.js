2d65058c66250d3423fd83275083411ee25ca018
Tiya Gallegos <tiyag@Tiyas-MacBook-Pro.local>
2020-04-21 23:31:10 -0600

    -m "Finished task 1"

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-//Task 1.1
+//Task 1.0 Select and cache the <main>element in a variable named mainEl.
 
 let mainEl = document.querySelector('main');
-mainEl.style.backGroundColor = 'var(--main-bg)';
 
-//Task 1.2
+//Task 1.1Set the background color of mainElto the value stored in the --main-bgCSS custom property.
+//Hint: Assign a string that uses the CSS var()function like this:
+//'var(--main-bg)'
+mainEl.style.backgroundColor = 'var(--main-bg)';
 
-mainEl.document.innerHTML = <h1>SEI Rocks!</h1>
+//Task 1.2 Set the content of mainElto <h1>SEI Rocks!</h1>
 
-//Task 1.3 
+mainEl.innerHTML = '<h1> SEI Rocks!</h1>'
 
+//Task 1.3 Add a class of flex-ctrto mainEl.
+
+mainEl.classList.add('flex-ctr');
